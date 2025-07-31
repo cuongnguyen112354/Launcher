c3dd7ccb758ab165bb3ac4a5bc52a87f3e9d0e59
cuongnguyen112354 <cuongnguyen112354@gmail.com>
2025-07-31 12:54:06 +0700

    Update UI

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Launcher.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Isla_Noctis {
+            get {
+                object obj = ResourceManager.GetObject("Isla Noctis", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap zoom {
             get {
                 object obj = ResourceManager.GetObject("zoom", resourceCulture);
